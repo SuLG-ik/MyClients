66a9a2d9143a2fb04dd2bc3fc023adc42b4300e0
vollllodya <nenashkinvov@gmail.com>
2022-04-04 17:16:20 +0700

    migrate to startup initialization of koin and logger + application

--- app/src/main/java/ru/shafran/cards/ShafranCardsApplication.kt
+++ app/src/main/java/ru/shafran/cards/ShafranCardsApplication.kt
@@ -1,13 +1,11 @@
 package ru.shafran.cards
 
 import android.app.Application
-import ru.shafran.startup.setupShafran
 
 class ShafranCardsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        setupShafran()
     }
 
 }
